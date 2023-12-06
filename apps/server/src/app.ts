c480cbf2a6a984a7c7bf19a5d048b0a65056538d
son.tran <son.tran@team.enouvo.com>
2023-12-06 19:39:58 +0700

    feat(server): add api sign up

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -15,6 +15,26 @@ app.use(
   })
 );
 
+app.post("/sign-up", async (c) => {
+  const { email, password } = await c.req.json();
+
+  if (!email || !password) {
+    return c.json(
+      { message: "Email and password are required", statusCode: 400 },
+      400
+    );
+  }
+
+  if (email !== "son.tran@enouvo.com" || password !== "enouvo12345") {
+    return c.json(
+      { message: "Invalid email or password", statusCode: 401 },
+      401
+    );
+  }
+
+  return c.json({ token: "124124124124124" });
+});
+
 app.get("/categories", (c) => {
   const categories = CategoriesService.getAll();
   return c.json(categories);
@@ -24,6 +44,16 @@ app.get("/categories/:categoryId/rooms", (c) => {
   const categoryId = c.req.param("categoryId");
   const rooms = RoomsService.getBy(categoryId);
   return c.json(rooms);
+});
+
+app.notFound((c) => {
+  return c.json(
+    {
+      message: "Not found",
+      statusCode: 404,
+    },
+    404
+  );
 });
 
 export default app;
